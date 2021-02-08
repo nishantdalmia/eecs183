1e22da66f867012a70590fb849a45e73cecdf537
Nishant Dalmia <ndalmia@umich.edu>
2021-02-07 19:12:09 -0500

    Project 2 files added.

---
+++ p2-rps/test.cpp
@@ -0,0 +1,98 @@
+/**
+ * test.cpp
+ * Project UID 24b93ce4a6274610f4c3f3e31fe17277t
+ *
+ * <#Name#>
+ * <#Uniqname#>
+ *
+ * EECS 183: Project 2
+ * Winter 2021
+ *
+ * Testing functions for your rps.cpp implementation.
+ * Holds the definitions of required testing functions.
+ * We have stubbed all required functions for you.
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+//************************************************************************
+// You should have implemented the following functions in rps.cpp
+//************************************************************************
+string getName(int playerNumber);
+int getMenuChoice();
+bool isMoveGood(char move);
+char getMove(string playerName);
+bool isRoundWinner(char move, char opponentMove);
+void announceRoundWinner(string winnerName);
+int doRound(string p1Name, string p2Name);
+void announceWinner(string winnerName);
+string doGame(string p1Name, string p2Name, int gameType);
+
+//************************************************************************
+// The following functions were already implemented for you in rps.cpp
+//************************************************************************
+void printInitialHeader();
+void printMenu();
+void printErrorMessage(int errorNumber);
+void printCloser();
+
+//************************************************************************
+// Testing function declarations. Function definition is below main.
+//************************************************************************
+void test_isMoveGood();
+void test_isRoundWinner();
+void test_announceRoundWinner();
+void test_announceWinner();
+
+
+void startTests() {
+    cout << "\nExecuting your test cases\n";
+
+    test_isMoveGood();
+    // TODO: call more test functions here
+
+    return;
+}
+
+//************************************************************************
+// Put all your test function implementations below here.
+// We have stubbed all required functions for you
+// to recieve full points when submitting test.cpp
+// NOTE: We will only grade your tests for the following functions
+//       * isMoveGood
+//       * isRoundWinner
+//       * announceRoundWinner()
+//       * announceWinner()
+//************************************************************************
+
+void test_isMoveGood() {
+    cout << "Now testing function isMoveGood()\n";
+    cout << "'r': Expected: 1, Actual: " << isMoveGood('r') << endl;
+    cout << "'q': Expected: 0, Actual: " << isMoveGood('q') << endl;
+
+    // TODO: implement more test cases here
+
+    return;
+}
+
+void test_isRoundWinner() {
+    // TODO: implement
+
+    return;
+}
+
+void test_announceRoundWinner() {
+	// TODO: implement
+    
+    return;
+}
+
+void test_announceWinner() {
+	// TODO: implement
+
+    return;
+}
